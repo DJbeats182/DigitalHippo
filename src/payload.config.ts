adc305a474f6b9f5f885e424c3c6929975ebbf34
DJ Bates <blooviper@gmail.com>
2023-12-11 17:10:37 -0700

    time for another save point, duration: 4h27m29s

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,22 +1,29 @@
 import { buildConfig } from "payload/config"
-import {webpackBundler} from "@payloadcms/bundler-webpack"
+import { webpackBundler } from "@payloadcms/bundler-webpack"
 import { mongooseAdapter } from "@payloadcms/db-mongodb"
-import {slateEditor} from "@payloadcms/richtext-slate"
+import { slateEditor } from "@payloadcms/richtext-slate"
 import path from "path"
+import { Users } from "./collections/Users"
+import dotenv from "dotenv"
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+})
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [],
+  collections: [Users],
   routes: {
     admin: "/sell",
   },
   admin: {
+    user: "users",
     bundler: webpackBundler(),
     meta: {
       titleSuffix: "- DigitalHippo",
       favicon: "/favicon.ico",
-      ogImage: "/thumbnail.jpg"
-    }
+      ogImage: "/thumbnail.jpg",
+    },
   },
   rateLimit: {
     max: 2000,
@@ -27,5 +34,5 @@ export default buildConfig({
   }),
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
-  }
+  },
 })
